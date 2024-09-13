3fea820f4c043409dd140038ffe68010311da199
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-09-13 13:14:31 +0800

    chart UI Linegraph, Bargraph => done

---
+++ resources/js/components/Chart_Bargraph.vue
@@ -0,0 +1,49 @@
+<script setup>
+import Chart from 'chart.js/auto'
+import { onMounted } from 'vue';
+
+function bargraph() {
+    const ctx = document.getElementById('myChart');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+            datasets: [{
+                label: '1 week Activity',
+                data: [300, 190, 300, 500, 200, 300, 23],
+                backgroundColor: [
+                    'rgba(139,0,0)',
+                    'rgba(165,42,42)',
+                    'rgba(178,34,34)',
+                    'rgba(220,20,60)',
+                    'rgba(255,0,0)',
+                    'rgba(255,99,71)',
+                    'rgba(255,127,80)'
+                ],
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+onMounted(() => {
+    bargraph()
+})
+</script>
+
+<template>
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+</template>
+<style scoped>
+#myChart {
+    max-height: 20rem;
+}
+</style>
--- resources/js/pages/client/Dashboard.vue
+++ resources/js/pages/client/Dashboard.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Sidebar from '@/components/Client_Sidebar.vue'
 import Header from '@/components/Client_Header.vue'
+import Bargraph from '@/components/Chart_Bargraph.vue'
+import Linegraph from '@/components/Chart_Line.vue'
 import { ref } from 'vue';
 
 const sidebar = ref(true)
@@ -8,6 +10,52 @@ const sidebar = ref(true)
 </script>
 
 <template>
-    <Header/>
-     
+    <Header />
+    <section id="section-one">
+        <div class="row mt-2 overview">
+            <div class="col">
+                <h1>Overview</h1>
+            </div>
+            <div class="col text-end">
+                <div class="balance">
+                    <h1>$5,343</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="section-two">
+        <div class="row">
+            <div class="col">
+                <Bargraph />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <Linegraph />
+            </div>
+        </div>
+    </section>
 </template>
+
+<style scoped>
+section {
+    width: 90%;
+    margin: auto;
+}
+
+#section-one h1 {
+    color: var(--bold-font-color);
+    font-weight: 600;
+    font-size: 20;
+}
+
+.overview {
+    background-color: rgb(250, 249, 249);
+    height: 5rem;
+    display: flex;
+    align-items: center;
+}
+
+.balance {}
+</style>
